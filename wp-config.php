e347892472586ee9e3eab56e1902b8b395c60313
TJ Rogers <tj.rogers7@gmail.com>
2017-08-11 22:40:20 -0400

    Macbook Pro wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'tjrwp76');
+define('DB_NAME', 'tjrwpdb');
 
 /** MySQL database username */
 define('DB_USER', 'tjrwpuser');
